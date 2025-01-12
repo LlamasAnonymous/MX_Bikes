93cca3c9d141a30ed985427f73060a27407b8139
LlamasAnonymous <gjbrooks2000@gmail.com>
2025-01-12 04:33:32 -0600

    Major Updates
    Added function that is called for each path requested

--- TransferBikes.ps1
+++ TransferBikes.ps1
@@ -1,55 +1,89 @@
 $ErrorActionPreference = 'SilentlyContinue'
-if ((Test-Path "$home\MXBikes\TracksLocation.txt") -and (Test-Path "$home\MXBikes\BikeSetupsLocation.txt") -and (Test-Path "$home\MXBikes\SetupsLocation.txt")) {
-} else {
+
+function filepath {
+    $filepath = Read-Host
+    $filepath = $filepath.Replace('"', '')
+
+    while ((Test-Path $filepath) -eq $false) {
+        cls
+
+        'Error'
+        Start-Sleep 1
+        "We could not find a valid location with: $filepath
+        "
+        $example
+        $filepath = Read-Host
+        $filepath = $filepath.Replace('"', '')
+    }
+    "Added $filepath"
+    $Global:filepath = $filepath
+}
+
+# Sets config file locations to variables
+$trackconf = "$home\MXBikes\TracksLocation.txt"
+$bikeconf = "$home\MXBikes\BikeSetupsLocation.txt"
+$setupsconf = "$home\MXBikes\SetupsLocation.txt"
+
+# Gets the stored paths
+$location1 = Get-Content $trackconf
+$location2 = Get-Content $bikeconf
+$location3 = Get-Content $setupsconf
+
+# Checks to see if the stored file paths are valid. This decides the outcome of the next if statement.
+if ((Test-Path $location1) -and (Test-Path $location2) -and (Test-Path $location3)) {
+    $locationcheck = $null
+
+    'Do you want to change any folder locations? (y,n)'
+    $change = Read-Host
+    if ($change -eq "y") {
+        $locationcheck = "check_failed"
+    }
+}
+else {
+    $locationcheck = "check_failed"
+}
+
+# If the stored location check fails, or the file paths don't exist, the script steps into this if statement
+if ((Test-Path $trackconf) -eq $false -or (Test-Path $bikeconf) -eq $false -or (Test-Path $setupsconf) -eq $false -or $locationcheck -eq "check_failed") {
+    # Deletes any old records of config files
+    rd "$home\MXBikes" -r -Force | Out-Null
     mkdir "$home\MXBikes" | Out-Null
 
-    'Where are your tracks stored?'
+    cls
+    
+    'Where are your tracks located?'
     'Example: "C:\Users\[Your user account]\Documents\PiBoSo\MX Bikes\mods\tracks"'
-    $trackfolder = Read-Host
 
-    while((Test-Path $trackfolder) -eq $false){
-        cls
-        "Are you sure this is the right path? We could not find this location: $trackfolder"
-        "Hint: Double-check the path that you entered, and make sure that you do NOT include quotation marks"
-        "Enter the correct Track folder path"
-        $trackfolder = Read-Host
-    }
-    $trackfolder | Out-File "$home\MXBikes\TracksLocation.txt"
+    $example = '    Where are your tracks located?
+    Example: "C:\Users\[Your user account]\Documents\PiBoSo\MX Bikes\mods\tracks"'
+    filepath
+
+    $Global:filepath | Out-File "$home\MXBikes\TracksLocation.txt"
     cls
 
     'What is the file path for your "BikeSetups" folder?'
     'Example: "C:\Users\[Your user account]\Documents\PiBoSo\MX Bikes\profiles\[Your profile]\BikeSetups"'
-    $bikesetupsfolder = Read-Host
+    $example = '    Where are your bike setups located?
+    Example: "C:\Users\[Your user account]\Documents\PiBoSo\MX Bikes\profiles\[Your profile]\BikeSetups"'
+    filepath
 
-    while((Test-Path $bikesetupsfolder) -eq $false){
-        cls
-        "Are you sure this is the right path? We could not find this location: $bikesetupsfolder"
-        "Hint: Double-check the path that you entered, and make sure that you do NOT include quotation marks"
-        "Enter the correct Bike Setups folder path"
-        $bikesetupsfolder = Read-Host
-    }
-    $bikesetupsfolder | Out-file "$home\MXBikes\BikeSetupsLocation.txt"
+    $Global:filepath | Out-file "$home\MXBikes\BikeSetupsLocation.txt"
     cls
     
     'What is the file path for your "setups" folder?'
     'Example: "C:\Users\[Your user account]\Documents\PiBoSo\MX Bikes\profiles\[Your profile]\setups"'
-    $setupsfolder = Read-Host
+    $example = '    Where are your saved setups located?
+    Example: "C:\Users\[Your user account]\Documents\PiBoSo\MX Bikes\profiles\[Your profile]\setups"'
+    filepath
 
-    while((Test-Path $setupsfolder) -eq $false){
-        cls
-        "Are you sure this is the right path? We could not find this location: $setupsfolder"
-        "Hint: Double-check the path that you entered, and make sure that you do NOT include quotation marks"
-        "Enter the correct Track Setups folder path"
-        $setupsfolder = Read-Host
-    }
-    $setupsfolder | Out-file "$home\MXBikes\SetupsLocation.txt"
+    $Global:filepath | Out-file "$home\MXBikes\SetupsLocation.txt"
     cls
 }
 'Loading...'
 
-$location1 = Get-Content "$home\MXBikes\TracksLocation.txt"
-$location2 = Get-Content "$home\MXBikes\BikeSetupsLocation.txt"
-$location3 = Get-Content "$home\MXBikes\SetupsLocation.txt"
+$location1 = Get-Content $trackconf
+$location2 = Get-Content $bikeconf
+$location3 = Get-Content $setupsconf
 
 $tracks = Get-ChildItem "$location1\*\*"
 $tracks = $tracks.BaseName
